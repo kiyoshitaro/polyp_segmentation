6f6eac7b96f4f477a32a717549ea471a2e51592a
brown-nth <hunglhp1998@gmail.com>
2021-05-06 00:45:20 +0700

    add submit

--- README.md
+++ README.md
@@ -15,6 +15,32 @@ CUDA_VISIBLE_DEVICES=0 python my_test.py -c configs/gcpa_gald_net_config.yaml
 
 ### [Dataloader](dataloader) (dataloader. name)
 
+All data must put in ./data, follow the tree:
+
+```bash
+.
+├── Kvasir_SEG_Training_880
+│   ├── images
+│   │   ├── cju0qkwl35piu0993l0dewei2.png
+│   │   ├── cju0qoxqj9q6s0835b43399p4.png
+│   └── masks
+│       ├── cju0qkwl35piu0993l0dewei2.png
+│       ├── cju0qoxqj9q6s0835b43399p4.png
+└── Kvasir_SEG_Validation_120
+    ├── images
+    │   ├── cju0s690hkp960855tjuaqvv0.png
+    │   ├── cju0sr5ghl0nd08789uzf1raf.png
+    └── masks
+        ├── cju0s690hkp960855tjuaqvv0.png
+        ├── cju0sr5ghl0nd08789uzf1raf.png
+```
+
+with BraTS2018 run:
+
+```bash
+    python utils/preprocess_nii.py
+```
+
 - KvasirDataset:
 
   - Kvasir-SEG: [test](https://drive.google.com/file/d/1us5iOMWVh_4LAiACM-LQa73t1pLLPJ7l/view?usp=sharing), [train](https://drive.google.com/file/d/17sUo2dLcwgPdO_fD4ySiS_4BVzc3wvwA/view?usp=sharing)
@@ -24,13 +50,25 @@ CUDA_VISIBLE_DEVICES=0 python my_test.py -c configs/gcpa_gald_net_config.yaml
 - ISICDataset:
 
   - [data](https://challenge.isic-archive.com/data#2018)
+  - Submit :
+    - 1000 images with name ISIC\_<image_id>\_segmentation.png
+      - 0: representing the background of the image, or areas outside the primary lesion
+      - 255: representing the foreground of the image, or areas inside the primary lesion
+    - metric : MeanIou
+    - [link](https://challenge.isic-archive.com/task/49)
 
 - USNerveDataset:
 
   - [data](https://www.kaggle.com/c/ultrasound-nerve-segmentation/data?fbclid=IwAR3Rly_-HfPylAAHSbEiX5a9Pt42VSXPwou4WEnuNHjl5GML5VOKrhLH2Ik)
+  - Submit:
+    - run mytest_usnerve.py
+    - 5508 images encoded with rle into submission.csv
 
 - BraTSDataset:
   - [data](https://www.med.upenn.edu/sbia/brats2018/data.html)
+  - Submit
+    - 66 file endwith .nii.gz
+    - [link](https://ipp.cbica.upenn.edu/jobs/306528931856371887)
 
 ### [Loss](network/optim/losses) (loss)
 
@@ -40,3 +78,7 @@ CUDA_VISIBLE_DEVICES=0 python my_test.py -c configs/gcpa_gald_net_config.yaml
 ### [Schedule](network/optim/schedulers.py) (scheduler)
 
 - cosine, cosine_warmup
+
+### Pretrain (put all in folder ./pretrained)
+
+[pretrain](https://drive.google.com/drive/folders/1RO6e7j3LRgGp2HQalZejxvVuKu0jZZPq?usp=sharing)
--- utils/preprocess_nii.py
+++ utils/preprocess_nii.py
@@ -26,41 +26,52 @@ def savepkl(data, path):
         pickle.dump(data, f)
 
 
-paths = [
-    os.path.join(p, p.split("/")[-1])
-    for p in glob.glob("data/MICCAI_BraTS_2018/Training/LGG/*")
-]
-from matplotlib.pyplot import imshow
+def convert_to_pkl(folder, save_path):
+    paths = [os.path.join(p, p.split("/")[-1]) for p in glob.glob(folder + "*")]
+    from matplotlib.pyplot import imshow
+
+    for path in paths:
+        label = np.array(nib_load(path + "_seg.nii.gz"), dtype="uint8", order="C")
+        images = np.stack(
+            [
+                np.array(
+                    nib_load(path + "_" + modal + ".nii.gz"), dtype="float32", order="C"
+                )
+                for modal in modalities
+            ],
+            -1,
+        )
+        mask = images.sum(-1) > 0
+        for k in range(4):
+            x = images[..., k]  #
+            y = x[mask]  #
+
+            lower = np.percentile(y, 0.2)  # 算分位数
+            upper = np.percentile(y, 99.8)
+
+            x[mask & (x < lower)] = lower
+            x[mask & (x > upper)] = upper
+            y = x[mask]
+            x -= y.mean()
+            x /= y.std()
+            images[..., k] = x
+
+        output = os.path.join(save_path, os.path.basename(path) + "data_f32.pkl")
+        print("saving:", output)
+        savepkl(data=(images, label), path=output)
 
 
-for path in paths:
-    label = np.array(nib_load(path + "_seg.nii.gz"), dtype="uint8", order="C")
-    images = np.stack(
-        [
-            np.array(
-                nib_load(path + "_" + modal + ".nii.gz"), dtype="float32", order="C"
-            )
-            for modal in modalities
-        ],
-        -1,
+if __name__ == "__main__":
+    # Merge HGG and LGG to train segmentation
+    convert_to_pkl(
+        "data/MICCAI_BraTS_2018/Training/LGG/",
+        "data/MICCAI_BraTS_2018/Training_pkl/",
     )
-    mask = images.sum(-1) > 0
-    for k in range(4):
-        x = images[..., k]  #
-        y = x[mask]  #
-
-        lower = np.percentile(y, 0.2)  # 算分位数
-        upper = np.percentile(y, 99.8)
-
-        x[mask & (x < lower)] = lower
-        x[mask & (x > upper)] = upper
-        y = x[mask]
-        x -= y.mean()
-        x /= y.std()
-        images[..., k] = x
-
-    output = (
-        "data/MICCAI_BraTS_2018/Training_pkl/" + os.path.basename(path) + "data_f32.pkl"
+    convert_to_pkl(
+        "data/MICCAI_BraTS_2018/Training/HGG/",
+        "data/MICCAI_BraTS_2018/Training_pkl/",
     )
-    print("saving:", output)
-    savepkl(data=(images, label), path=output)
+    convert_to_pkl(
+        "data/MICCAI_BraTS_2018/Validate/",
+        "data/MICCAI_BraTS_2018/Validate_pkl/",
+    )
