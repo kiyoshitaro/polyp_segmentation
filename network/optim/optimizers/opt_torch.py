523ec539f7cde05c1513b491bd81f7539dbaccff
kiyoshitaro <hunglhp1998@gmail.com>
2022-01-20 22:33:26 +0700

    add adamp optimize

--- network/optim/optimizers/opt_torch.py
+++ network/optim/optimizers/opt_torch.py
@@ -1,4 +1,5 @@
 import torch
+from adamp import AdamP
 
 
 def adam(params, lr):
@@ -7,3 +8,6 @@ def adam(params, lr):
 
 def sgd(params, lr):
     return torch.optim.SGD(params, lr=lr)
+
+def adamp(params, lr):
+    return AdamP(params, lr=lr, betas=(0.9, 0.999), weight_decay=1e-2)
